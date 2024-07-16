ff911085fcd34e1bffb3a7c027320954413551c7
Henriquehead <carloshenriquegastronomia@gmail.com>
2024-07-16 05:44:59 -0300

    adicionando forms com js

---
+++ script.js
@@ -0,0 +1,10 @@
+let formulario = document.querySelector(".formulario");
+
+function showForm(){
+    formulario.style.left = "50%"
+}
+
+function hideForm(){
+    formulario.style.left = "-50%"
+}
+
